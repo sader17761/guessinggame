808fbb9125285815926a7d2f14b37670a8e35fa8
Parker Walker <parkerwalker@Parkers-MacBook-Pro.local>
2017-05-26 14:45:54 -0500

    started dropdown

--- public/scripts/random.js
+++ public/scripts/random.js
@@ -3,10 +3,18 @@ $(onReady);
 function onReady(){
   console.log('jq');
   $('#startButton').on('click', startGame);
+  $('#resetButton').on('click', resetGame);
+}
+
+function resetGame(){
+  console.log('button reset');
+  $('#setup').show();
+  $('#play').hide();
 }
 
 function startGame(){
   console.log('button start');
   $('#setup').hide();
   $('#play').show();
+  console.log($('#maxNumberIn').val());
 }
