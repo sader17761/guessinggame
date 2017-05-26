c502a1d6d898defbc6c89317c814f5a5dd6eb8c3
Parker Walker <parkerwalker@Parkers-MacBook-Pro.local>
2017-05-26 14:16:59 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//requires area
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(2345, function(){
+    console.log('server is up on 2345');
+})
